c9bd2f83ec214b9b35b332f62530fd91d0999cb3
StevenXu <steven_xu98@outlook.com>
2017-05-28 20:34:23 +0800

    sync to tp

--- hash.c
+++ hash.c
@@ -11,33 +11,37 @@ void hash_create1(int *a, int n, int *hash, int m)
 {
 	int i, j, flag = 1; 
 	for(i = 0; i < n; i++){
-		if(hash[a[i] mod m] == -1){
-			hash[(a[i] mod m)] = a[i];
+		if(hash[a[i] % m] == -1){
+			hash[(a[i] % m)] = a[i];
+			printf("one :%d in %d \n", a[i], a[i]%m);
 		}else{
-			for(j = 1, j < m, ++j){
-				if(hash[(a[i]+j) mod m] == -1){
-					hash[(a[i]+j) mod m] == a[i];
+			for(j = 0; j < m; ++j){
+				if(hash[(a[i]+j) % m] == -1){
+					hash[(a[i]+j) % m] == a[i];
+					printf("two :%d in %d \n", a[i], a[i]%m);
 					flag = 0;
 					break;
 				} //if
 			}// for
 			if(flag){
-				printf("hash is full");
+				printf("hash is full ");
+				
 				return ;
 			} //if
 		} //else
 	} //for
+	for(i = 0; i < n; i++) printf("%d ", hash[i]);
 } //hash_create1
 
 void hash_search1(int *hash, int m, int key)
 {
-	int d
+	int d;
 	for(d = 0; d < m; ++d){
-		if(hash[key mod m+d] == -1){
+		if(hash[key % m+d] == -1){
 			printf("no found ");
 			return ;
 		}
-		if(hash[key mod m+d] == key){
+		if(hash[key % m+d] == key){
 			printf("found %d ", key);
 			return;
 		}
@@ -46,41 +50,49 @@ void hash_search1(int *hash, int m, int key)
 	return ;
 }
 
-void hash_create2(int *a, int n, Lhash *hash, int m)
-{
-	int i ;
-	Phash temp = (Phash)malloc(sizeof(Lhash));
-	temp->next = NULL;
-	for(i = 0; i < n; i++){
-		temp->data = a[i];
-		if(hash[a[i] mod m]->data != -1){
-			hash[a[i] mod m]->next = temp;
-		}else{
-			hash[a[i] mod m]->data = a[i];
-		} //else
-	} // for
-} // hash_create2
+// void hash_create2(int *a, int n, Phash hash, int m)
+// {
+// 	int i ;
+// 	Phash temp = (Phash)malloc(sizeof(Lhash));
+// 	temp->next = NULL;
+// 	for(i = 0; i < n; i++){
+// 		temp->data = a[i];
+// 		if(hash[a[i] % m]->data != -1){
+// 			hash[a[i] % m]->next = temp;
+// 		}else{
+// 			hash[a[i] % m]->data = a[i];
+// 		} //else
+// 	} // for
+// } // hash_create2
 
-void hash_search2(Lhash *hash, int key)
-{
-	while(hash[key mod m]){
-		if(hash[key mod m]->data == -1){
-			printf("no found");
-			return ;
-		}
-		if(hash[key mod m]->data == key){
-			printf("found %d", key);
-			return ;
-		}
-		hash[key mod m] = hash[key mod m]->next;
-	}
-	printf("no found");
-	return ;
-}
+// void hash_search2(Phash hash, int key, int m)
+// {
+// 	while(hash[key % m]->next){
+// 		if(hash[key % m]->data == -1){
+// 			printf("no found");
+// 			return ;
+// 		}
+// 		if(hash[key % m]->data == key){
+// 			printf("found %d", key);
+// 			return ;
+// 		}
+// 		hash[key % m] = hash[key % m]->next;
+// 	}
+// 	printf("no found");
+// 	return ;
+// }
 
 int main()
 {
-	
+	int hash[5] = {-1, -1, -1, -1, -1};
+	int i;
+	for(i = 0; i < 5; i++) printf("%d ", hash[i]);
+	int array[5] = {121,132,122,133,135};
+
+	hash_create1(array, 5, hash, 5);
+	getchar();
+	hash_search1(array, 5, 121);
+	getchar();
 
 	return 0;
 }
